c4798809741d78fa461e7c8f572881119b47c268
daclv0711 <daclv0711@gmail.com>
2022-03-17 09:22:56 +0700

    update title

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children, title, description }) => (
       <meta charSet="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
+      <title>thu title nexts test | Home</title>
       <link rel="canonical" href="https://nextjs-daclv0711.vercel.app/" />
       <meta
         name="description"
