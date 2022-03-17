be930516b7b57e1a8a06c1f192a1fde098bf6c4b
daclv0711 <daclv0711@gmail.com>
2022-03-17 09:13:09 +0700

    add keywords

--- components/Layout.js
+++ components/Layout.js
@@ -13,23 +13,23 @@ const Layout = ({ children, title, description }) => (
       <meta
         name="description"
         content={
-          "NextJS là một framework được xây dựng dựa trên React. Next.Js hỗ trợ server-side rendered mà không cần phải cấu hình gì."
+          "NextJS là một framework được xây dựng dựa trên React. Next.Js hỗ trợ server-side rendered mà không cần phải cấu hình gì ..."
         }
       />
       <meta name="author" content="next app" />
-      <meta name="keywords" content="next app" />
+      <meta name="keywords" content="next app, nexts asaasba" />
       <meta name="robots" content="index, follow" />
       <meta name="googlebot" content="index, follow" />
       <meta name="google" content="notranslate" />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta
         property="og:description"
         content={
-          "NextJS là một framework được xây dựng dựa trên React. Next.Js hỗ trợ server-side rendered mà không cần phải cấu hình gì."
+          "NextJS là một framework được xây dựng dựa trên React. Next.Js hỗ trợ server-side rendered mà không cần phải cấu hình gì ..."
         }
       />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
       <meta property="og:url" content="https://nextjs-daclv0711.vercel.app/" />
       <meta
         property="og:site_name"
