15d773b7175f483b99beef98d3a682469f870f70
Lê Văn Đắc <84674027+daclv0711@users.noreply.github.com>
2022-07-29 10:31:20 +0700

    Update random.js

--- pages/jokes/random.js
+++ pages/jokes/random.js
@@ -44,7 +44,8 @@ export const getStaticProps = async () => {
     props: {
       joke,
     },
-    revalidate: 60, // 60s => reload lai trang
+    revalidate: 1, // 60s => reload lai trang
+    useCdn: false,
   };
 };
 
